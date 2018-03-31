8f53f0bcbe14745f61314db1ef7ea25b4f2f1ede
missetand <c.nielse39@gmail.com>
2018-03-31 12:50:24 +0200

    added user-cards and rounting

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+
 
 
 @NgModule({
   imports: [
     MatButtonModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule
   ],
   exports: [
     MatButtonModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
